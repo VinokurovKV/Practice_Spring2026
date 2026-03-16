f28e403588c6d01c04e3ef6b70981bfab51e8875
Kirill <kirikvin8490@gmail.com>
2026-03-16 14:30:33 +0300

    Corrected client version

--- 20260316/0/dullnetcat.py
+++ 20260316/0/dullnetcat.py
@@ -2,7 +2,7 @@ import sys
 import socket
 import cmd
 
-class NC(Cmd.cmd):
+class NC(cmd.Cmd):
     prompt = "> "
 
     def do_connect(self, arg):
@@ -11,18 +11,27 @@ class NC(Cmd.cmd):
         match args:
             case [host, port]:
                 self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.s.connect((nost, int(port)))
+                self.s.connect((host, int(port)))
+                print("Connected")
 
     def do_print(self, arg):
         if arg:
-            self.s.sendall(b"print " + arg.encode())
+            self.s.sendall(b"print " + arg.encode() + b'\n')
             print(self.s.recv(1024).rstrip().decode())
 
     def do_info(self, arg):
+        """Info host | port"""
         match arg:
             case "host" | "port":
-                self.s.sendall(b"info " + arg.encode())
+                self.s.sendall(b"info " + arg.encode() + b'\n')
                 print(self.s.recv(1024).rstrip().decode())
 
-    def complete_info(text, line, begidx, endidx):
+    def complete_info(self, text, line, begidx, endidx):
         return [s for s in ["host", "port"] if s.startswith(text)]
+
+    def do_EOF(self, arg):
+        return 1
+
+
+if __name__ == "__main__":
+    NC().cmdloop()
